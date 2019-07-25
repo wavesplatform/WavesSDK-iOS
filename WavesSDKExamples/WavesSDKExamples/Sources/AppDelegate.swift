bcbab967fce6d45c9bb9e4899ef907c5516a982f
Mefilt <Mefilt@gmail.com>
2019-07-25 12:41:07 +0300

    Example move to other project

---
+++ WavesSDKExamples/WavesSDKExamples/Sources/AppDelegate.swift
@@ -0,0 +1,33 @@
+//
+//  AppDelegate.swift
+//  WavesSDK
+//
+//  Created by rprokofev on 03/04/2019.
+//  Copyright © 2019 Waves. All rights reserved.
+//
+
+import UIKit
+import WavesSDK
+import WavesSDKExtensions
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        SweetLogger.current.add(plugins: [SweetLoggerConsole(visibleLevels: [.network, .error], isShortLog: false)])
+        SweetLogger.current.visibleLevels = [.network, .error]
+        
+        WavesSDK.initialization(servicesPlugins: .init(data: [],
+                                                       node: [],
+                                                       matcher: []),
+                                enviroment: .init(server: .mainNet, timestampServerDiff: 0))
+        
+        
+                
+        return true
+    }
+}
