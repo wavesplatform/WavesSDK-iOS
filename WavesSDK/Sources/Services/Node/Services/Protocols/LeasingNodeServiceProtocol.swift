5d42aecbd38a07327c10274f66de68af4011fe2a
Mefilt <Mefilt@gmail.com>
2021-05-25 11:36:09 +0300

    Added new response

--- WavesSDK/Sources/Services/Node/Services/Protocols/LeasingNodeServiceProtocol.swift
+++ WavesSDK/Sources/Services/Node/Services/Protocols/LeasingNodeServiceProtocol.swift
@@ -14,6 +14,6 @@ public protocol LeasingNodeServiceProtocol {
      Active leasing transactions of account
      - Parameter: address Address
 */
-    func leasingActiveTransactions(by address: String) -> Observable<[NodeService.DTO.LeaseTransaction]>
+    func leasingActiveTransactions(by address: String) -> Observable<[NodeService.DTO.LeaseResponse]>
     
 }
